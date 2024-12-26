949fd3394e8db8e312a09ffe96a5859e1a369058
jwt388 <jtnh388@gmail.com>
2024-12-26 12:51:28 -0500

    Update to 2025 Beta 3. Undo feedforward change.

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -4,9 +4,6 @@
 
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.RPM;
-import static edu.wpi.first.units.Units.Volts;
-
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -17,7 +14,6 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -132,8 +128,7 @@ public class MotorSubsystem extends SubsystemBase implements AutoCloseable {
       // Calculate the the motor command by adding the PID controller output and feedforward to run
       // the motor at the desired speed. Store the individual values for logging.
       pidOutput = motorController.calculate(getSpeed());
-      AngularVelocity velocityRpm = RPM.of(motorController.getSetpoint());
-      newFeedforward = feedforward.calculate(velocityRpm).in(Volts);
+      newFeedforward = feedforward.calculate(motorController.getSetpoint());
       motor.setVoltage(pidOutput + newFeedforward);
 
     } else {
